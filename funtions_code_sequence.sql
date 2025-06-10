5a90ec8ce36d6433169d877bf1c83213f3921e88
Irsyad <irsyadrisno@gmail.com>
2025-06-10 14:57:57 +0700

    code_funtions

---
+++ funtions_code_sequence.sql
@@ -0,0 +1,28 @@
+CREATE SEQUENCE public.code_inventaris_asset_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+--funtions
+
+CREATE OR REPLACE FUNCTION public.f_add_sequence_t_code_inventaris_asset()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    -- Format angka dengan awalan 'TS' dan leading zero (misal: PO0001, PO0002)
+   	NEW."nomor_inventaris" := 'INV' || LPAD(nextval('code_inventaris_asset_seq')::text, 5, '0');
+    RETURN NEW;
+END;
+$function$
+;
+
+-- trigger untuk memamnggil funtions
+
+create trigger t_generate_sequence_t_inventaris_asset_seq before
+insert
+    on
+    public.t_inventaris_asset for each row execute function f_add_sequence_t_code_inventaris_asset();
