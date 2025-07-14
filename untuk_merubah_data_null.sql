8e9ebd9eb850d27b01087ae78559791490a8249d
04irsyaD <irsyadrisno@gmail.com>
2025-07-14 16:24:33 +0700

    update all table

--- untuk_merubah_data_null.sql
+++ untuk_merubah_data_null.sql
@@ -16,3 +16,28 @@ BEGIN
         );
     END LOOP;
 END$$;
+
+
+-- query untuk mengubah data null pada all table
+
+DO $$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN
+        SELECT table_schema, table_name, column_name
+        FROM information_schema.columns
+        WHERE table_schema = 'public'
+          AND data_type IN ('character varying', 'text', 'char')
+    LOOP
+        RAISE NOTICE 'Memproses: %.% (kolom: %)', r.table_schema, r.table_name, r.column_name;
+
+        EXECUTE format(
+            'UPDATE %I.%I SET %I = NULL WHERE %I = '''';',
+            r.table_schema,
+            r.table_name,
+            r.column_name,
+            r.column_name
+        );
+    END LOOP;
+END$$;
