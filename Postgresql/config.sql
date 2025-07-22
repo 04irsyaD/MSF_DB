d660624c3a37f0388d35e0a6f56799a00fe14b15
04irsyaD <irsyadrisno@gmail.com>
2025-07-22 15:51:32 +0700

    hisstory of query

--- Postgresql/config.sql
+++ Postgresql/config.sql
@@ -5,4 +5,18 @@ SHOW shared_buffers;
 SHOW config_file;
 
 -- version information
-select version()
+select version();
+
+-- check history of queries
+
+SELECT pid, usename, query, state_change
+FROM pg_stat_activity
+WHERE query_start > NOW() - INTERVAL '12 hour'
+ORDER BY query_start DESC;
+
+-- check for idle transactions
+
+SELECT query, state, usename, application_name, client_addr, backend_start
+FROM pg_stat_activity
+WHERE query_start > NOW() - INTERVAL '12 hour'
+ORDER BY query_start DESC;
