c8ce3d4c280843857bb5745b39f6e2dd35549d82
04irsyaD <irsyadrisno@gmail.com>
2025-07-23 08:35:21 +0700

    flust host mysql

--- Mysql/config.sql
+++ Mysql/config.sql
@@ -1,3 +1,7 @@
 -- MySQL Configuration Script
 select version() as mysql_version;
 select @@global.sql_mode as global_sql_mode, @@session.sql_mode as session_sql_mode;
+
+
+--  mysqladmin flush-hosts
+FLUSH HOSTS;
