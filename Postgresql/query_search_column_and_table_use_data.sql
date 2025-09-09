4ce6bbca4602ef07d68ead2bbef1cec2f1de9672
04irsyaD <irsyadrisno@gmail.com>
2025-09-09 14:41:48 +0700

    after_POSTOP

---
+++ Postgresql/query_search_column_and_table_use_data.sql
@@ -0,0 +1,12 @@
+SELECT 
+    schemaname,
+    tablename,
+    attname as column_name,
+    n_distinct,
+    correlation
+FROM pg_stats 
+WHERE schemaname = 'public' 
+AND (
+    most_common_vals::text LIKE '%irsyad%' 
+    OR histogram_bounds::text LIKE '%irsyad%'
+);
