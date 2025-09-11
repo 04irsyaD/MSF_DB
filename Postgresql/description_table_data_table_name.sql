451297331c341bb043930788f9dccbf2e04e8ec8
04irsyaD <irsyadrisno@gmail.com>
2025-09-11 16:16:33 +0700

    description_database

---
+++ Postgresql/description_table_data_table_name.sql
@@ -0,0 +1,17 @@
+-- This SQL query retrieves detailed information about all columns in all tables
+SELECT 
+    c.table_name,
+    c.column_name,
+    c.data_type,
+    c.character_maximum_length,
+    c.is_nullable,
+    pgd.description AS column_description
+FROM information_schema.columns c
+LEFT JOIN pg_catalog.pg_statio_all_tables st
+       ON c.table_schema = st.schemaname 
+      AND c.table_name = st.relname
+LEFT JOIN pg_catalog.pg_description pgd 
+       ON pgd.objoid = st.relid 
+      AND pgd.objsubid = c.ordinal_position
+WHERE c.table_schema = 'public'
+ORDER BY c.table_name, c.ordinal_position;
