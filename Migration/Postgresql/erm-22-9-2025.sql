adcae7e0b48dbacbe7374b2e4bfec6d46ecb8904
04irsyaD <irsyadrisno@gmail.com>
2025-09-23 16:08:40 +0700

    update masterdatav1

--- Migration/Postgresql/erm-22-9-2025.sql
+++ Migration/Postgresql/erm-22-9-2025.sql
@@ -18,6 +18,11 @@ BEGDATE         ENDATE           ACSID                                      ACSC
 2025-09-09      2999-12-31      "d66d9696-a053-42d2-99bc-834e4679bc77"      "risk_treatment__pic_deadline" "No specific treatment"          "Risk Treatment Pic Deadline"             2025-09-09 13:34:14.072 +0700        2025-09-09        Jihan/Irsyad
 2025-09-09      2999-12-31      "d66d9696-a053-42d2-99bc-834e4679bc77"      "risk_treatment__only_pic_deadline" "Considered treatment"      "Risk Treatment Only Pic Deadline"        2025-09-09 13:34:14.072 +0700        2025-09-09        Jihan/Irsyad
 
+add master data t_menu_accces
+BEGDATE		 ENDATE           MNACID                                     MNACCD                                     				MNCD                               ACSD          	 					CRAT           						CHGDA					CHGBY	
+2025-09-09  2999-12-31      "01cf59ba-f97d-40ae-9b00-11e92f00d741"      "menu_risk_reporting__risk_treatment__only_pic_deadline"   menu_risk_reporting       		"risk_treatment__only_pic_deadline"     2025-09-09 13:28:32.382 +0700        2025-09-09        Jihan/Irsyad
+2025-09-09  2999-12-31      "0efade78-3360-41d5-aee1-9c96b851f52c"      "menu_risk_reporting__risk_treatment__pic_deadline"  	   menu_risk_reporting      		"risk_treatment__pic_deadline"        	2025-09-09 13:28:32.382 +0700		 2025-09-09        Jihan/Irsyad
+
 CREATE TABLE public.t_form_risklist (
 	"BEGDA" date DEFAULT CURRENT_DATE NULL,
 	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
@@ -156,6 +161,92 @@ CREATE TABLE public.t_list_grisklist (
 	"X6" varchar(255) NULL,
 	"X7" varchar(255) NULL
 );
+
+CREATE TABLE public.t_log_gkeylist (
+	"BEGDA" date DEFAULT CURRENT_DATE NULL,
+	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
+	"LGKEYID" uuid DEFAULT uuid_generate_v4() NULL,
+	"CRAT" timestamp(6) NULL,
+	"CHGDA" timestamp(6) NULL,
+	"CHGBY" varchar(150) NULL,
+	"X1" varchar(255) NULL,
+	"X2" varchar(255) NULL,
+	"X3" varchar(255) NULL,
+	"X4" varchar(255) NULL,
+	"X5" varchar(255) NULL,
+	"X6" varchar(255) NULL,
+	"X7" varchar(255) NULL,
+	"ENFOD" varchar(255) NULL,
+	"ENFOR" varchar(255) NULL,
+	"ID_GKEY" int4 NULL,
+	"STATCD" varchar(255) DEFAULT NULL::character varying NULL
+);
+
+CREATE TABLE public.t_log_grisklist (
+	"BEGDA" date DEFAULT CURRENT_DATE NULL,
+	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
+	"LGRLID" varchar DEFAULT uuid_generate_v4() NULL,
+	"GRILID" uuid NULL,
+	"RISKCD" varchar NULL,
+	"PRD" date NULL,
+	"VRSN" int4 NULL,
+	"STATCD" varchar NULL,
+	"OLSTCD" varchar NULL,
+	"NOTES" text NULL,
+	"CRAT" timestamptz(6) DEFAULT CURRENT_TIMESTAMP NULL,
+	"CHGDA" timestamptz(6) DEFAULT CURRENT_TIMESTAMP NULL,
+	"CHGBY" varchar(150) NULL,
+	"X1" varchar NULL,
+	"X2" varchar NULL,
+	"X3" varchar NULL,
+	"X4" varchar NULL,
+	"X5" varchar NULL,
+	"X6" varchar NULL,
+	"X7" varchar NULL
+);
+
+CREATE TABLE public.t_log_ikeylist (
+	"BEGDA" date DEFAULT CURRENT_DATE NULL,
+	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
+	"LIKEYID" uuid DEFAULT uuid_generate_v4() NULL,
+	"CRAT" timestamp(6) NULL,
+	"CHGDA" timestamp(6) NULL,
+	"CHGBY" varchar(150) NULL,
+	"X1" varchar(255) NULL,
+	"X2" varchar(255) NULL,
+	"X3" varchar(255) NULL,
+	"X4" varchar(255) NULL,
+	"X5" varchar(255) NULL,
+	"X6" varchar(255) NULL,
+	"X7" varchar(255) NULL,
+	"ENFOD" varchar(255) NULL,
+	"ENFOR" varchar(255) NULL,
+	"ID_IKEY" int4 NULL,
+	"STATCD" varchar(255) DEFAULT NULL::character varying NULL
+);
+
+CREATE TABLE public.t_log_treatment (
+	"BEGDA" date DEFAULT CURRENT_DATE NULL,
+	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
+	"LTREID" uuid DEFAULT uuid_generate_v4() NULL,
+	"PICIDS" varchar NULL,
+	"DEADLI" date NULL,
+	"EVID" varchar NULL,
+	"CRAT" timestamp(6) NULL,
+	"CHGDA" timestamp(6) NULL,
+	"CHGBY" varchar(150) NULL,
+	"TRILID" varchar(255) NULL,
+	"X2" varchar(255) NULL,
+	"X3" varchar(255) NULL,
+	"X4" varchar(255) NULL,
+	"X5" varchar(255) NULL,
+	"X6" varchar(255) NULL,
+	"X7" varchar(255) NULL,
+	"SNBUCD" varchar(255) NULL
+);
+
+add master dataa
+
 
 alter table t_risk_champion 
 add COLUMN
