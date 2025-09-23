a1a323f555a59e71b6b2d8a112b8bc274db92fa6
04irsyaD <irsyadrisno@gmail.com>
2025-09-23 11:07:37 +0700

    update migration

--- Migration/Postgresql/erm-22-9-2025.sql
+++ Migration/Postgresql/erm-22-9-2025.sql
@@ -18,8 +18,24 @@ BEGDATE         ENDATE           ACSID                                      ACSC
 2025-09-09      2999-12-31      "d66d9696-a053-42d2-99bc-834e4679bc77"      "risk_treatment__pic_deadline" "No specific treatment"          "Risk Treatment Pic Deadline"             2025-09-09 13:34:14.072 +0700        2025-09-09        Jihan/Irsyad
 2025-09-09      2999-12-31      "d66d9696-a053-42d2-99bc-834e4679bc77"      "risk_treatment__only_pic_deadline" "Considered treatment"      "Risk Treatment Only Pic Deadline"        2025-09-09 13:34:14.072 +0700        2025-09-09        Jihan/Irsyad
 
-
-last udapte in table t_evidencele
+CREATE TABLE public.t_form_risklist (
+	"BEGDA" date DEFAULT CURRENT_DATE NULL,
+	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
+	"TFRLID" uuid DEFAULT uuid_generate_v4() NULL,
+	"BUCD" varchar NULL,
+	"PRD" date NULL,
+	"VRSN" int4 DEFAULT 0 NULL,
+	"CRAT" timestamptz(6) DEFAULT CURRENT_TIMESTAMP NULL,
+	"CHGDA" timestamptz(6) NULL,
+	"CHGBY" varchar(150) NULL,
+	"X1" varchar NULL,
+	"X2" varchar NULL,
+	"X3" varchar NULL,
+	"X4" varchar NULL,
+	"X5" varchar NULL,
+	"X6" varchar NULL,
+	"X7" varchar NULL
+);
 
 alter table t_risk_champion 
 add COLUMN
