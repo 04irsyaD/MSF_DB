249637f266eecd4c745872e333513c1ce552fe27
04irsyaD <irsyadrisno@gmail.com>
2025-10-07 14:16:33 +0700

    we add tables

--- Migration/Postgresql/erm_to_prod.sql
+++ Migration/Postgresql/erm_to_prod.sql
@@ -10,4 +10,38 @@
 
 -- Step 1: Add New Tables and columns to PROD schema
 
-FUNCTION to PROCEDURE
+-- insert ADD UPDATE table prd 
+
+CREATE TABLE public.t_form_risklist (
+	"BEGDA" date DEFAULT CURRENT_DATE NULL,
+	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
+	"TFRLID" uuid DEFAULT uuid_generate_v4() NULL,
+	"BUCD" varchar NULL,
+	"PRD" date NULL,
+	"VRSN" int4 DEFAULT 0 NULL,
+	"CRAT" timestamptz(6) DEFAULT CURRENT_TIMESTAMP NULL,
+	"CHGDA" timestamptz(6) NULL,
+	"CHGBY" varchar(150) NULL,
+	"X1" varchar NULL,
+	"X2" varchar NULL,
+	"X3" varchar NULL,
+	"X4" varchar NULL,
+	"X5" varchar NULL,
+	"X6" varchar NULL,
+	"X7" varchar NULL
+);
+
+CREATE TABLE public.t_grisk_notes (
+	"BEGDA" date DEFAULT CURRENT_DATE NULL,
+	"ENDDA" date DEFAULT '2999-01-01'::date NULL,
+	"GRNID" uuid DEFAULT uuid_generate_v4() NULL,
+	"RISKCD" varchar NULL,
+	"PRD" date NULL,
+	"VRSN" int4 NULL,
+	"NIK" varchar NULL,
+	"NOTES" text NULL,
+	"SOURCE" varchar NULL,
+	"GRILID" varchar NULL,
+	"CRAT" timestamptz(6) DEFAULT CURRENT_DATE NULL,
+	"CHGDA" timestamptz(6) DEFAULT CURRENT_DATE NULL
+);
