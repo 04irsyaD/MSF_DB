b5548fdb6f9ecde5129bdac636e10a097745e4bc
04irsyaD <irsyadrisno@gmail.com>
2026-02-09 13:44:45 +0700

    wowo engine db

---
+++ Postgresql/index/search_all_index.sql
@@ -0,0 +1,9 @@
+-- code search all indexk in database
+-- Semua table
+SELECT 
+    tablename,
+    indexname,
+    indexdef
+FROM pg_indexes
+WHERE schemaname = 'public'
+ORDER BY tablename, indexname;
