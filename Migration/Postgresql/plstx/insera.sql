8c7f18860812a6227d783330bc9b4bef9a71eb85
04irsyaD <irsyadrisno@gmail.com>
2026-05-21 11:35:03 +0700

    udpate

--- Migration/Postgresql/plstx/insera.sql
+++ Migration/Postgresql/plstx/insera.sql
@@ -14,8 +14,8 @@ CREATE TABLE public.t_t_insera (
 	updated_at timestamptz(6) NULL,
 	deleted_at timestamptz(6) NULL,
     is_active bool NULL,
-    constraint t_t_insera_id_pk PRIMARY KEY (id)
-    CONSTRAINT fk_status_pm_preventive FOREIGN KEY (pm_status_id) REFERENCES public.pm_status(id),
+    constraint t_t_insera_id_pk PRIMARY KEY (id),
+    CONSTRAINT fk_status_ticket_insera FOREIGN KEY (status_ticket_insera_id) REFERENCES public.t_m_status_ticket_insera(id)
 );
 
 create table t_m_status_ticket_insera (
@@ -41,4 +41,32 @@ CREATE table t_log_ticket_insera (
     created_at timestamptz(6) DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamptz(6) NULL,
     deleted_at timestamptz(6) NULL,
+    is_active bool NULL,
+    constraint t_log_ticket_insera_id_pk PRIMARY KEY (id),
+    CONSTRAINT fk_t_ticket_insera FOREIGN KEY (t_ticket_insera_id) REFERENCES public.t_t_insera(id),
+    CONSTRAINT fk_status_ticket_insera_log FOREIGN KEY (status_ticket_insera_id) REFERENCES public.t_m_status_ticket_insera(id)
+);
+
+
+
+
+CREATE TABLE public.t_t_rating_logon_detail (
+	id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    t_t_rating_logon_id uuid NOT NULL,
+	logon_shift_id uuid NULL,
+	rating_value int4 NULL,
+	note_rating text NULL,
+	t_ticket_id int4 NULL,
+	created_by_id uuid NOT NULL,
+	updated_by_id uuid NULL,
+	created_at timestamptz(6) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	updated_at timestamptz(6) NULL,
+	deleted_at timestamptz(6) NULL,
+	is_active bool DEFAULT true NOT NULL,
+	rate_by_id uuid NULL,
+	rated_at timestamptz(6) NULL,
+	CONSTRAINT t_t_rating_logon_Detail_pk PRIMARY KEY (id),
+    constraint fk_t_t_rating_logon_id FOREIGN KEY (t_t_rating_logon_id) REFERENCES public.t_t_rating_logon(id) ON DELETE CASCADE,
+	CONSTRAINT fk_t_t_rating_logon_logon_shift_id FOREIGN KEY (logon_shift_id) REFERENCES public.t_t_logon_shift(id) ON DELETE CASCADE,
+	CONSTRAINT fk_t_t_rating_logon_ticket_id FOREIGN KEY (t_ticket_id) REFERENCES public.t_ticket(id) ON DELETE CASCADE
 );
